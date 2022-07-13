a919bddf06d018dd8695e64281249fddfc491ae3
kissolga0904 <kissolga0904@gmail.com>
2022-07-13 12:10:03 +0200

    section 5: create javascript dropdown for categories

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function (){
     return view('posts',[
-        'posts' => Post::latest()->get()
+        'posts' => Post::latest()->get(),
+        'categories' => Category::all()
     ]);
 });
 
@@ -34,12 +35,15 @@ Route::get('posts/{post:slug}',function (Post $post){
 //new route to show all posts in one category:
 Route::get('categories/{category:slug}',function (Category $category){
     return view('posts',[
-        'posts'=>$category->posts
+        'posts'=>$category->posts,
+        'currentCategory' => $category,
+        'categories' => Category::all()
         ]);
 });
 
 Route::get('authors/{author:username}',function (User $author){
     return view('posts',[
-        'posts'=>$author->posts
+        'posts'=>$author->posts,
+        'categories' => Category::all()
     ]);
 });
