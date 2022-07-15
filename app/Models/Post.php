f71e14850af892db20b86b4ff6e6bc6bca612f7e
kissolga0904 <qlcbh6@std.uni-eszterhazy.hu>
2022-07-15 09:33:48 +0200

    section 6: wip

---
+++ app/Models/Post.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    //everything is fillable, except what you provide to this array
+    protected $guarded = [];
+
+    //default for every post query you perform
+    protected $with = ['category', 'author'];
+
+    //the other things will be ignored, we just care about the title, excerpt and the body
+    //protected $fillable = ['title', 'excerpt', 'body', 'id'];
+    /* not neccessary in this case
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+    */
+    public function scopeFilter($query, array $filters){
+        $query->when($filters['search'] ?? false, fn ($query,$search)=>
+            $query
+                ->where('title', 'like', '%' . $search. '%')
+                ->orWhere('body', 'like', '%' . $search . '%'));
+    }
+
+    public function category(){
+        //relationship between a post and a category
+        return $this ->belongsTo(Category::class);
+    }
+
+    public function author()
+    {
+        return $this ->belongsTo(User::class, 'user_id');
+    }
+}
