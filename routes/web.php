c0219253a6249debdacef9db3484cff0db01d7d7
kissolga0904 <qlcbh6@std.uni-eszterhazy.hu>
2022-07-18 13:46:04 +0200

    section 7: added eloquent query constraints

--- routes/web.php
+++ routes/web.php
@@ -24,13 +24,13 @@ Route::get('/',[PostController::class,'index'])->name('home');
 Route::get('posts/{post:slug}',[PostController::class,'show']);
 
 //new route to show all posts in one category:
-Route::get('categories/{category:slug}',function (Category $category){
+/*Route::get('categories/{category:slug}',function (Category $category){
     return view('posts',[
         'posts'=>$category->posts,
         'currentCategory' => $category,
         'categories' => Category::all()
         ]);
-})->name('category');
+})->name('category');*/
 
 Route::get('authors/{author:username}',function (User $author){
     return view('posts',[
