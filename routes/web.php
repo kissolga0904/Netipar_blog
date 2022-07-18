554a2ab07254846c4429f8bd6e57cf0fd125bc34
kissolga0904 <qlcbh6@std.uni-eszterhazy.hu>
2022-07-18 14:38:34 +0200

    section 7: added author filtering

--- routes/web.php
+++ routes/web.php
@@ -7,19 +7,6 @@ use App\Models\User;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
 
-
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/',[PostController::class,'index'])->name('home');
 Route::get('posts/{post:slug}',[PostController::class,'show']);
 
@@ -32,8 +19,8 @@ Route::get('posts/{post:slug}',[PostController::class,'show']);
         ]);
 })->name('category');*/
 
-Route::get('authors/{author:username}',function (User $author){
-    return view('posts',[
+/*Route::get('authors/{author:username}',function (User $author){
+    return view('posts.index',[
         'posts'=>$author->posts
     ]);
-});
+});*/
