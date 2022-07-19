94733bc6668787332c1290c251fba8fee597f909
kissolga0904 <qlcbh6@std.uni-eszterhazy.hu>
2022-07-19 08:48:00 +0200

    section 9: created registration

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\RegisterController;
 use App\Models\Post;
 use App\Models\Category;
 use App\Models\User;
@@ -10,6 +11,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/',[PostController::class,'index'])->name('home');
 Route::get('posts/{post:slug}',[PostController::class,'show']);
 
+Route::get('register',[RegisterController::class,'create']);
+Route::post('register',[RegisterController::class,'store']);
 //new route to show all posts in one category:
 /*Route::get('categories/{category:slug}',function (Category $category){
     return view('posts',[
