9f0f1c642999269cc0cc56d31661d688c4bd5291
kissolga0904 <qlcbh6@std.uni-eszterhazy.hu>
2022-07-21 16:43:22 +0200

    section 11: mailchimp api tinkering

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,22 @@ use App\Http\Controllers\PostCommentsController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionsController;
-use App\Models\Post;
-use App\Models\Category;
-use App\Models\User;
-use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
+
+Route::get('ping', function (){
+    $mailchimp = new MailchimpMarketing\ApiClient();
+
+    $mailchimp->setConfig([
+        'apiKey' => config('services.mailchimp.key'),
+        'server' => 'us13'
+    ]);
+
+    $response = $mailchimp->lists->addListMember('53dd251d1a',[
+        'email_address'=>'olga2001@indamail.hu',
+        'status'=>'subscribed'
+    ]);
+    ddd($response);
+});
 
 Route::get('/',[PostController::class,'index'])->name('home');
 Route::get('posts/{post:slug}',[PostController::class,'show']);
