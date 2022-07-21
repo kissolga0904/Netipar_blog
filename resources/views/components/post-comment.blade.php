779abc71ab7ffed4ad715faa4ab99b3656baab8a
kissolga0904 <qlcbh6@std.uni-eszterhazy.hu>
2022-07-21 10:40:52 +0200

    section 10: create the comment section

---
+++ resources/views/components/post-comment.blade.php
@@ -0,0 +1,19 @@
+<article class="flex bg-gray-100 p-6 border border-gray-200 rounded-xl space-x-4">
+    <div class="flex-shrink-0">
+        <img src="https://i.pravatar.cc/100" alt="" width="60" height="60" class="rounded-xl">
+    </div>
+    <div>
+        <header class="mb-4">
+            <h3 class="font-bold">John Doe</h3>
+            <p class="text-xs">
+                Posted
+                <time>8 months ago </time>
+            </p>
+        </header>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse elementum suscipit metus id elementum.
+            Donec non condimentum dolor, id tristique tortor. In in turpis pretium, tincidunt neque eu, posuere ex.
+            In hac habitasse platea dictumst. Nulla rhoncus augue ante, at posuere nibh varius eget.
+        </p>
+    </div>
+</article>
