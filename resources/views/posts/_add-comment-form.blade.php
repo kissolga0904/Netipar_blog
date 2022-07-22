a339596252f1ec629eb88c79c79a250305d7c3ee
kissolga0904 <qlcbh6@std.uni-eszterhazy.hu>
2022-07-22 14:33:08 +0200

    section 12: extract form-specific blade components

--- resources/views/posts/_add-comment-form.blade.php
+++ resources/views/posts/_add-comment-form.blade.php
@@ -20,9 +20,9 @@
             </div>
 
             <div class="flex justify-end mt-6 border-t border-gray-200 pt-6">
-                <x-submit-button>
+                <x-form.button>
                     Post
-                </x-submit-button>
+                </x-form.button>
             </div>
         </form>
     </x-panel>
