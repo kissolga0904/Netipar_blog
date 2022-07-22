4ee88913cac45779e53f93616698fadf160045fd
kissolga0904 <qlcbh6@std.uni-eszterhazy.hu>
2022-07-22 17:55:22 +0200

    section 12: create a form to edit and delete posts

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminPostController;
 use App\Http\Controllers\NewsletterController;
 use App\Http\Controllers\PostCommentsController;
 use App\Http\Controllers\PostController;
@@ -23,11 +24,16 @@ Route::get('login', [SessionsController::class,'create'])->middleware('guest');
 Route::post('login', [SessionsController::class,'store'])->middleware('guest');
 Route::post('logout', [SessionsController::class,'destroy'])->middleware('auth');
 
-Route::get('admin/posts/create',[PostController::class, 'create'])->middleware('admin');
-Route::post('admin/posts',[PostController::class, 'store'])->middleware('admin');
+Route::get('admin/posts',[AdminPostController::class, 'index'])->middleware('admin');
+Route::get('admin/posts/create',[AdminPostController::class, 'create'])->middleware('admin');
+Route::get('admin/posts/create',[AdminPostController::class, 'create'])->middleware('admin');
+Route::get('admin/posts/{post}/edit',[AdminPostController::class, 'edit'])->middleware('admin');
+Route::patch('admin/posts/{post}',[AdminPostController::class, 'update'])->middleware('admin');
+Route::delete('admin/posts/{post}',[AdminPostController::class, 'destroy'])->middleware('admin');
 //new route to show all posts in one category:
 /*Route::get('categories/{category:slug}',function (Category $category){
     return view('posts',[
+
         'posts'=>$category->posts,
         'currentCategory' => $category,
         'categories' => Category::all()
